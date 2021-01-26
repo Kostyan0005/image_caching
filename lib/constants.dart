b5758e41f31679bf557f9b79393d40df67381951
Kostyan0005 <rkd000521@gmail.com>
2021-01-26 19:06:17 +0200

    Added layout and image caching

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+const kImageUrls = [
+  'https://onlandscape-pullzone-heyqa02xvhohdyshyl51qdqk.netdna-ssl.com/wp-content/uploads/2012/01/IMG_6347-square-vertorama.jpg?iv=285',
+  'https://i1.wp.com/digital-photography-school.com/wp-content/uploads/2013/07/bs-01.jpg?w=600&h=1260&ssl=1',
+  'https://previews.123rf.com/images/tainar/tainar1802/tainar180200029/96517059-beautiful-lily-flower-blooming-bouquet-square-background-greeting-card-template-toned-image-spring-n.jpg',
+  'https://i.pinimg.com/originals/f0/01/32/f00132f86f28dd16ef6a5641a954c3fc.jpg',
+  'https://images.all-free-download.com/images/graphicthumb/beautiful_scenery_02_hd_picture_166319.jpg',
+  'https://pbs.twimg.com/profile_images/1098225250469326848/2KN00JjZ_400x400.png',
+  'https://www.lovethispic.com/uploaded_images/153035-Beautiful-Winter-Night.jpg',
+  'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ec39c139-c59e-4678-9917-e7b5f4026b64/d4m46e7-1bab27f5-0046-4d2b-b27f-bd920dc30741.jpg/v1/fill/w_900,h_900,q_75,strp/purple_mars_by_sogrepcorpus_d4m46e7-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD05MDAiLCJwYXRoIjoiXC9mXC9lYzM5YzEzOS1jNTllLTQ2NzgtOTkxNy1lN2I1ZjQwMjZiNjRcL2Q0bTQ2ZTctMWJhYjI3ZjUtMDA0Ni00ZDJiLWIyN2YtYmQ5MjBkYzMwNzQxLmpwZyIsIndpZHRoIjoiPD05MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.Ug3H-qr__p2fsvq6_RtvLqMw4PGxTAfBZwPsrMpIdgA',
+  'https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2007/02/image_of_mars_seen_by_osiris_a_cloudy_day_on_mars/10258961-2-eng-GB/Image_of_Mars_seen_by_OSIRIS_A_cloudy_day_on_Mars.jpg',
+  'https://flutter.dev/assets/homepage/carousel/slide_1-bg-4e2fcef9a7343692a5f7784d68241a786c57c79d55f0fe37e6b82a653d146b93.jpg',
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_caching/constants.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,7 +12,63 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Placeholder(),
+      home: ListView.builder(
+        padding: EdgeInsets.zero,
+        itemCount: kImageUrls.length,
+        itemBuilder: (_, index) => ListImage(index),
+      ),
     );
+  }
+}
+
+class ListImage extends StatelessWidget {
+  const ListImage(this.index);
+
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return Container(
+      width: width,
+      height: width,
+      child: FutureBuilder(
+        future: _getCachedImage(index),
+        builder: (_, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting)
+            return Center(
+              child: SizedBox(
+                width: 50,
+                height: 50,
+                child: CircularProgressIndicator(),
+              ),
+            );
+
+          if (snapshot.hasError)
+            return Center(
+              child: Text('Error while loading image with index $index'),
+            );
+
+          return FittedBox(
+            child: Image.file(snapshot.data),
+            fit: BoxFit.fill,
+          );
+        },
+      ),
+    );
+  }
+
+  Future<File> _getCachedImage(int index) async {
+    final temp = await getTemporaryDirectory();
+    final imageFile = File('${temp.path}/images/$index');
+    if (imageFile.existsSync()) return imageFile; // return if exists
+
+    final response = await http.get(kImageUrls[index]);
+    if (response.statusCode == 200)
+      return imageFile
+        ..createSync(recursive: true)
+        ..writeAsBytesSync(response.bodyBytes);
+    else
+      throw Exception();
   }
 }
